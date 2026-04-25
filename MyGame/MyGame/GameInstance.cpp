c62624a57addc53b009977c48ca5e6d48dee89de
Deguchi <nt8>
2026-04-25 22:30:06 +0900

    no message

--- MyGame/MyGame/GameInstance.cpp
+++ MyGame/MyGame/GameInstance.cpp
@@ -267,7 +267,7 @@ bool GameInstance::CreateAssets(ID3D11Device* device)
 {
     if (!m_baseShader.Load(device, L"VertexShader.hlsl", L"PixelShader.hlsl")) return false;
 
-    if (!myModel.LoadFromFile(device, "C:/Users/PC_User/Documents/DirectX/MyGame/MyGame/Asset/test2/test.pmx")) 
+    if (!myModel.LoadFromFile(device, "Asset/test2/test.pmx")) 
     {
         MessageBox(nullptr, L"PMXの読み込みに失敗しました。パスを確認してください。", L"Error", MB_OK);
         return false;
